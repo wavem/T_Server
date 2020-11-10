121fe7ebdfffe3fb50449d0273b9d55d725e7326
wavem <fenix24@naver.com>
2020-11-10 17:13:37 +0900

    [Add] Tray Icon & Basic Frame Work
    - 기본 와꾸 설정 : 리본 메뉴 및 노트북 페이지 설정
    - Tray Icon 으로 변환하는 기능 추가 : Tray Icon 더블클릭 루틴 및 종료 메뉴 삽입.
    - 프로그램 아이콘 설정함.
    - 끝. 커밋하겠음.

--- Tetris_Server/Main.h
+++ Tetris_Server/Main.h
@@ -7,15 +7,103 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Menus.hpp>
+#include "cxClasses.hpp"
+#include "cxControls.hpp"
+#include "cxGraphics.hpp"
+#include "cxLookAndFeelPainters.hpp"
+#include "cxLookAndFeels.hpp"
+#include "dxBar.hpp"
+#include "dxRibbon.hpp"
+#include "dxRibbonCustomizationForm.hpp"
+#include "dxRibbonSkins.hpp"
+#include "dxSkinBlack.hpp"
+#include "dxSkinBlue.hpp"
+#include "dxSkinBlueprint.hpp"
+#include "dxSkinCaramel.hpp"
+#include "dxSkinCoffee.hpp"
+#include "dxSkinDarkRoom.hpp"
+#include "dxSkinDarkSide.hpp"
+#include "dxSkinDevExpressDarkStyle.hpp"
+#include "dxSkinDevExpressStyle.hpp"
+#include "dxSkinFoggy.hpp"
+#include "dxSkinGlassOceans.hpp"
+#include "dxSkinHighContrast.hpp"
+#include "dxSkiniMaginary.hpp"
+#include "dxSkinLilian.hpp"
+#include "dxSkinLiquidSky.hpp"
+#include "dxSkinLondonLiquidSky.hpp"
+#include "dxSkinMcSkin.hpp"
+#include "dxSkinMetropolis.hpp"
+#include "dxSkinMetropolisDark.hpp"
+#include "dxSkinMoneyTwins.hpp"
+#include "dxSkinOffice2007Black.hpp"
+#include "dxSkinOffice2007Blue.hpp"
+#include "dxSkinOffice2007Green.hpp"
+#include "dxSkinOffice2007Pink.hpp"
+#include "dxSkinOffice2007Silver.hpp"
+#include "dxSkinOffice2010Black.hpp"
+#include "dxSkinOffice2010Blue.hpp"
+#include "dxSkinOffice2010Silver.hpp"
+#include "dxSkinOffice2013DarkGray.hpp"
+#include "dxSkinOffice2013LightGray.hpp"
+#include "dxSkinOffice2013White.hpp"
+#include "dxSkinPumpkin.hpp"
+#include "dxSkinsCore.hpp"
+#include "dxSkinsDefaultPainters.hpp"
+#include "dxSkinsdxBarPainter.hpp"
+#include "dxSkinsdxRibbonPainter.hpp"
+#include "dxSkinSeven.hpp"
+#include "dxSkinSevenClassic.hpp"
+#include "dxSkinSharp.hpp"
+#include "dxSkinSharpPlus.hpp"
+#include "dxSkinSilver.hpp"
+#include "dxSkinSpringTime.hpp"
+#include "dxSkinStardust.hpp"
+#include "dxSkinSummer2008.hpp"
+#include "dxSkinTheAsphaltWorld.hpp"
+#include "dxSkinValentine.hpp"
+#include "dxSkinVisualStudio2013Blue.hpp"
+#include "dxSkinVisualStudio2013Dark.hpp"
+#include "dxSkinVisualStudio2013Light.hpp"
+#include "dxSkinVS2010.hpp"
+#include "dxSkinWhiteprint.hpp"
+#include "dxSkinXmas2008Blue.hpp"
+#include "AdvSmoothButton.hpp"
 //---------------------------------------------------------------------------
-class TForm1 : public TForm
+class TFormMain : public TForm
 {
 __published:	// IDE-managed Components
+	TTrayIcon *TrayIcon;
+	TPopupMenu *PopupMenu;
+	TMenuItem *Exit1;
+	TPanel *__pnBase;
+	TNotebook *Notebook_Main;
+	TPanel *_pnBase_01_Status;
+	TdxRibbonTab *RibbonMenuTab_Main;
+	TdxRibbon *RibbonMenu;
+	TdxBarManager *BarMgr;
+	TdxBar *BarMgrBar;
+	TdxBarLargeButton *MenuBtn_Status;
+	TdxBarLargeButton *MenuBtn_Setting;
+	TdxBarLargeButton *MenuBtn_Version;
+	TPanel *_pnBase_02_Setting;
+	TAdvSmoothButton *btn_Hide;
+	void __fastcall Exit1Click(TObject *Sender);
+	void __fastcall TrayIconDblClick(TObject *Sender);
+	void __fastcall MenuBtn_StatusClick(TObject *Sender);
+	void __fastcall MenuBtn_SettingClick(TObject *Sender);
+	void __fastcall btn_HideClick(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
-	__fastcall TForm1(TComponent* Owner);
+	__fastcall TFormMain(TComponent* Owner);
+
+
+public: // START
+	void __fastcall InitProgram();
 };
 //---------------------------------------------------------------------------
-extern PACKAGE TForm1 *Form1;
+extern PACKAGE TFormMain *FormMain;
 //---------------------------------------------------------------------------
 #endif
